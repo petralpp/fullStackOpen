aa2b4fde4d2e6acfe5785614f06cd1b3dc69f152
Petra <petra.paloniemi94@hotmail.com>
2025-06-19 13:40:50 +0300

    4.2 backend toteutettu

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,44 +1,8 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const app = require('./app')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const url = process.env.MONGODB_URI
-
-console.log('Connecting to database')
-mongoose.connect(url).then(() => {
-  console.log('Connected to MongoDB')
-}).catch((error) => {
-  console.log('Error connecting to MongoDB:', error.message)
-})
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  }).catch(error => response.status(400).send({ error: error.message }))
-})
-
-const PORT = 3003
+const PORT = config.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
