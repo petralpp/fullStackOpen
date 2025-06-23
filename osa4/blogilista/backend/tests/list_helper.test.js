05663185056ac113334abd9ef9a5bf5443bd59b2
Petra <petra.paloniemi94@hotmail.com>
2025-06-23 15:34:35 +0300

    Tehtävä 4.4 totalLikes

--- osa4/blogilista/backend/tests/list_helper.test.js
+++ osa4/blogilista/backend/tests/list_helper.test.js
@@ -1,10 +1,39 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const { test_blogs } = require('../utils/test_blogs')
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+    ]
+
+    test('of empty list is zero', () => {
+        const result = listHelper.totalLikes([])
+        assert.strictEqual(result, 0)
+    })
+
+    test('when list has only one blog equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(test_blogs)
+        assert.strictEqual(result, 36)
+    })
 })
