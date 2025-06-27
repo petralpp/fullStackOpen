1732412dd1f8452368145b0ad7656a456b7daba6
Petra <petra.paloniemi94@hotmail.com>
2025-06-27 11:36:33 +0300

    4.9 testi: id oikeassa muodossa

--- osa4/blogilista/backend/tests/blog_api.test.js
+++ osa4/blogilista/backend/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -8,21 +8,32 @@ const { initialBlogs } = require('./test_helper')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(initialBlogs)
-})
+describe('when there are blogs initially saved', () => {
+    beforeEach(async () => {
+      await Blog.deleteMany({})
+      await Blog.insertMany(initialBlogs)
+    })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+    test('blogs are returned as json', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
 
-test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, initialBlogs.length)
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
+        assert.strictEqual(response.body.length, initialBlogs.length)
+    })
+
+    test('id fields are in correct form', async () => {
+        const response = await api.get('/api/blogs')
+        response.body.forEach(b => {
+            assert(Object.hasOwn(b, 'id'))
+            assert(!Object.hasOwn(b, '_id'))
+    })
+    })
+
 })
 
 after(async () => {
