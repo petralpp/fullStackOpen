bbb56d9661fe4fb7a561d3e837252b7e91f276fc
Petra <petra.paloniemi94@hotmail.com>
2025-08-09 17:27:36 +0300

    5.19 testi: blogin lisäys onnistuu

--- osa5/e2e-cypress/cypress/e2e/blog_app.cy.js
+++ osa5/e2e-cypress/cypress/e2e/blog_app.cy.js
@@ -9,10 +9,10 @@ describe('Blog app', () => {
     }
     cy.request('POST', 'http://localhost:3003/api/users/', user)
   })
-  it('Login form is shown', function () {
+  it('Login form is shown', function() {
     cy.contains('Log in to application')
   })
-  describe('Login',function() {
+  describe('Login', function() {
     it('succeeds with correct credentials', function() {
       cy.get('#username').type('mluukkai')
       cy.get('#password').type('salainen')
@@ -24,6 +24,22 @@ describe('Blog app', () => {
       cy.get('#password').type('salainen')
       cy.contains('Login').click()
       cy.contains('invalid username or password')
+    })
+  })
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.contains('Login').click()
+    })
+    it('a blog can be created', function() {
+      cy.contains('New blog').click()
+      cy.get('#title').type('The Test Blog')
+      cy.get('#author').type('The Tester')
+      cy.get('#url').type('http://www.tester.com')
+      cy.contains('Add').click()
+
+      cy.contains('The Test Blog The Tester')
     })
   })
 })
