7614bac53cb6828502ab5bafaf4bf7bf52c85f39
Petra <petra.paloniemi94@hotmail.com>
2025-09-01 15:23:51 +0300

    7.5 kenttien reset

--- osa7/routed-anecdotes/src/components/CreateNew.jsx
+++ osa7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,26 +1,29 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useField } from "../hooks"
 
 const CreateNew = (props) => {
-  //const [content, setContent] = useState('')
-  //const [author, setAuthor] = useState('')
-  //const [info, setInfo] = useState('')
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const contentField = useField('text')
+  const authorField = useField('text')
+  const infoField = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
+      content: contentField.value,
+      author: authorField.value,
+      info: infoField.value,
       votes: 0
     })
     navigate('/')
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    contentField.reset()
+    authorField.reset()
+    infoField.reset()
   }
 
   return (
@@ -29,17 +32,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input {...contentField} />
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...authorField} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...infoField} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
